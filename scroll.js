b85e045fd20bcd3334363825ab38802870b61cf9
Aloxbro <alox51@wanadoo.fr>
2019-09-20 09:22:36 +0200

    hotfix mobile footer

--- scroll.js
+++ scroll.js
@@ -1,23 +1,5 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
-<<<<<<< HEAD
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-=======
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
@@ -31,5 +13,4 @@ function scrollFunction() {
 function topFunction() {
  document.body.scrollTop = 0; // For Safari
  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
->>>>>>> dev
 }
