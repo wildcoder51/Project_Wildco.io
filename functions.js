c5140d9580b1c3d11d200af465bbd5cb173707dd
Hugo V <eyleteam@gmail.com>
2019-10-04 10:22:51 +0200

    on commit rien

--- functions.js
+++ functions.js
@@ -2,11 +2,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-​
 function fn () {
   //Get the button
   const mybutton = document.getElementById("Btn");
-​
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
   function scrollFunction() {
@@ -17,21 +15,17 @@ function fn () {
     }
   }
   // When the user clicks on the button, scroll to the top of the document
-​
-​
-  const flexBox1 = document.getElementsByClassName('flex1');
-  const flexBox2 = document.getElementsByClassName('flex2');
-​
-​
-​
-​
+  
 }
-​
+
+/*
 function domReady(fn) {
   document.addEventListener("DOMContentLoaded", fn);
   if (document.readyState === "interactive" || document.readyState === "complete" ) {
     fn();
-  }
+  };
+
 }
-​
+
 domReady(fn)
+*/
